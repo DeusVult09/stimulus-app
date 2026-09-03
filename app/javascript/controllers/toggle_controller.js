c57c88ec3da98b820fa2b5bc86e717b88ef79722
Zarifa <mamadalievazara@gmail.com>
2026-09-04 01:22:52 +0200

    Add hidden element function

--- app/javascript/controllers/toggle_controller.js
+++ app/javascript/controllers/toggle_controller.js
@@ -1,10 +1,18 @@
 import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
-  static targets = [ "list" ]
+  static targets = [ "list", "visible", "invisible" ]
 
   toggle() {
     this.listTarget.hidden = !this.listTarget.hidden
   }
+  
+
+  switch() {
+    this.visibleTarget.hidden = !this.visibleTarget.hidden
+    this.invisibleTarget.hidden = !this.invisibleTarget.hidden
+  }
+
+
 
 }
